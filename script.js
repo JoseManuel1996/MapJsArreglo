325cf8d1cdeb2c9a520d5cfdce9627a2a5e85f1b
JoseManuel1996 <josemanuelosocat@gmail.com>
2023-08-08 16:32:08 -0500

    Arreglo con MAP

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ alert("Hola Mundo, Qué faciñ es incluir commilas simples y dobles como estas "
 
 
 console.log("Hola Manuel ")
+
+const Arreglo1 = ["Manuel", "osorio", "Catalan"]
+Arreglo1.map((item, index)=>{
+    console.log(index + ":" +item)
+})  
